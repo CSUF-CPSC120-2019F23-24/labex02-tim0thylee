ca6bf567606aae009608a0700f8d40cecf2af413
Timothy Lee <student@cs104u-2f.acad.fullerton.edu>
2019-08-31 13:27:12 -0700

    completed pob01

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -1,10 +1,18 @@
-// Name: 
+// Name: Timothy Lee
 // This program displays a diamond pattern to the screen.
 
 #include <iostream>
 
 int main()
 {
-
+  //Use std out to display each row.
+  //Each \n represents a new row.
+  std::cout << "   *   \n";
+  std::cout << "  ***  \n";
+  std::cout << " ***** \n";
+  std::cout << "*******\n";
+  std::cout << " ***** \n";
+  std::cout << "  ***  \n";
+  std::cout << "   *   \n";
   return 0;
 }
